30311161292734876665cc642d806d89d56c62c2
Muaz Amir Nur Rashid <muazamir99@gmail.com>
2025-09-23 09:22:10 +0800

    remove redirects

--- next.config.ts
+++ next.config.ts
@@ -27,17 +27,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
-        destination: 'https://www.hashtech.app/:path*',
-        permanent: true,
-      },
-    ]
-  },
 };
 
 export default nextConfig;
