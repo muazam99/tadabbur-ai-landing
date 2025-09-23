0decdefbd5976b04f8dcef833882f863d534e2f4
Muaz Amir Nur Rashid <muazamir99@gmail.com>
2025-09-23 08:58:23 +0800

    remove vercel

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,17 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
+        destination: 'https://www.tadabbur-ai.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
